09842eae9e6d01636199582e66bcfd97a77f3b4c
Lim-YongKwan <dydrhks02@naver.com>
2022-06-05 01:11:18 +0900

    27_commit
    27번 별찍기 9 commit

--- BOJ_2446/BOJ_2446/main.swift
+++ BOJ_2446/BOJ_2446/main.swift
@@ -12,6 +12,14 @@ var N = Int(readLine()!)!
 for i in 1...2*N-1{
     
     for j in stride(from: 1, through: 2*(N-i), by: 1){
+    for k in stride(from: 1, to: N-abs(N-i), by: 1){
+        print(" ",terminator: "")
+    }
+    
+    for j in stride(from: 0, through: 2*abs((N-i)), by: 1){
         print("*",terminator: "")
     }
+   
+    
+    print()
 }
